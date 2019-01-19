8f2a201a8a0fd97abeec4b7a71e8efafd9123e5a
Naman <agrahari.nam@gmail.com>
2019-01-19 12:30:55 +0000

    bucket animation

---
+++ resources/views/bucket/1.blade.php
@@ -0,0 +1,115 @@
+<div class="col-sm-4">
+  @php
+  $myview = App\Bucket::where('roll', Auth::user()->rollno)->where('list', 1)->get()->toArray();
+  @endphp
+  @if(!empty($myview))
+  <aside class="profile-card" style="background: url({{$myview[0]['pic']}}); background-size: 100% 100%; background-repeat: no-repeat;">
+    <header>
+      <h1>SF Salsa</h1><i class="fa fa-check-circle" style="color: #5cb85c; font-size: 30px;" title="Task Completed!"></i>
+      <h2>Swayed to the beats</h2><br>
+      <h2> of Salsa in SF</h2>
+    </header>
+    <div class="profile-bio" style="text-align: center;">
+      <form action="/bucketpost/1" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="file" required="required" name="fileToUpload" >
+        <br><br>
+        <input type="submit" value="Change" name="submit" class="btn btn-success" align="right">
+      </form>
+    </div>
+  </aside>
+  <br>
+  @else
+  <aside class="profile-card" style="background: url(https://s-media-cache-ak0.pinimg.com/736x/3a/1f/02/3a1f02c10700daa80655a302c5a27acf.jpg);">
+    <header>
+      <h1>SF Salsa</h1>
+      <h2>Swayed to the beats</h2><br>
+      <h2> of Salsa in SF</h2>
+    </header>
+    <div class="profile-bio" style="text-align: center;">
+      <form action="/bucketpost/1" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="file" required="required" name="fileToUpload" >
+        <br><br>
+        <input type="submit" value="Upload Image" name="submit" class="btn btn-success" align="right">
+      </form>
+    </div>
+  </aside><br>
+  @endif
+</div>
+
+<div class="col-sm-4">
+  @php
+  $myview = App\Bucket::where('roll', Auth::user()->rollno)->where('list', 2)->get()->toArray();
+  @endphp
+  @if(!empty($myview))
+  <aside class="profile-card" style="background: url({{$myview[0]['pic']}}); background-size: 100% 100%; background-repeat: no-repeat;">
+    <header>
+      <h1>Old Archi Building</h1><i class="fa fa-check-circle" style="color: #5cb85c; font-size: 30px;" title="Task Completed!"></i>
+      <h2>Felt the horror in Old</h2><br>
+      <h2> Archi Building</h2>
+    </header>
+    <div class="profile-bio" style="text-align: center;">
+      <form action="/bucketpost/2" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="file" required="required" name="fileToUpload" >
+        <br><br>
+        <input type="submit" value="Change" name="submit" class="btn btn-success" align="right">
+      </form>
+    </div>
+  </aside><br>
+  @else
+  <aside class="profile-card" style="background: url(https://s-media-cache-ak0.pinimg.com/736x/3a/1f/02/3a1f02c10700daa80655a302c5a27acf.jpg);">
+    <header>
+      <h1>Old Archi Building</h1>
+      <h2>Felt the horror in Old</h2><br>
+      <h2> Archi Building</h2>
+    </header>
+    <div class="profile-bio" style="text-align: center;">
+      <form action="/bucketpost/2" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="file" required="required" name="fileToUpload" >
+        <br><br>
+        <input type="submit" value="Upload Image" name="submit" class="btn btn-success" align="right">
+      </form>
+    </div>
+  </aside><br>
+  @endif
+</div>
+
+<div class="col-sm-4">
+  @php
+  $myview = App\Bucket::where('roll', Auth::user()->rollno)->where('list', 3)->get()->toArray();
+  @endphp
+  @if(!empty($myview))
+  <aside class="profile-card" style="background: url({{$myview[0]['pic']}}); background-size: 100% 100%; background-repeat: no-repeat;">
+    <header>
+      <h1>Graffitis</h1><i class="fa fa-check-circle" style="color: #5cb85c; font-size: 30px;" title="Task Completed!"></i>
+      <h2>Sketched graffiti in hall.</h2>
+    </header>
+    <div class="profile-bio" style="text-align: center;">
+      <form action="/bucketpost/3" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="file" required="required" name="fileToUpload" >
+        <br><br>
+        <input type="submit" value="Change" name="submit" class="btn btn-success" align="right">
+      </form>
+    </div>
+  </aside><br>
+  @else
+  <aside class="profile-card" style="background: url(https://s-media-cache-ak0.pinimg.com/736x/3a/1f/02/3a1f02c10700daa80655a302c5a27acf.jpg);">
+    <header>
+      <h1>Graffitis</h1>
+      <h2>Sketched graffiti in hall.</h2>
+    </header>
+    <div class="profile-bio" style="text-align: center;">
+      <form action="/bucketpost/3" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="file" required="required" name="fileToUpload" >
+        <br><br>
+        <input type="submit" value="Upload Image" name="submit" class="btn btn-success" align="right">
+      </form>
+    </div>
+  </aside><br>
+  @endif
+</div>
---
+++ resources/views/bucket/3.blade.php
@@ -0,0 +1,110 @@
+<div class="col-sm-4">
+  @php
+  $myview = App\Bucket::where('roll', Auth::user()->rollno)->where('list', 7)->get()->toArray();
+  @endphp
+  @if(!empty($myview))
+  <aside class="profile-card" style="background: url({{$myview[0]['pic']}}); background-size: 100% 100%; background-repeat: no-repeat;">
+    <header>
+      <h1>Treat</h1><i class="fa fa-check-circle" style="color: #5cb85c; font-size: 30px;" title="Task Completed!"></i>
+      <h2>Treat at Kolaghat</h2>
+    </header>
+    <div class="profile-bio" style="text-align: center;">
+      <form action="/bucketpost/7" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="file" required="required" name="fileToUpload" >
+        <br><br>
+        <input type="submit" value="Change" name="submit" class="btn btn-success" align="right">
+      </form>
+    </div>
+  </aside><br>
+  @else
+  <aside class="profile-card" style="background: url(https://s-media-cache-ak0.pinimg.com/736x/3a/1f/02/3a1f02c10700daa80655a302c5a27acf.jpg);">
+    <header>
+      <h1>Treat</h1>
+      <h2>Treat at Kolaghat</h2>
+    </header>
+    <div class="profile-bio" style="text-align: center;">
+      <form action="/bucketpost/7" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="file" required="required" name="fileToUpload" >
+        <br><br>
+        <input type="submit" value="Upload Image" name="submit" class="btn btn-success" align="right">
+      </form>
+    </div>
+  </aside><br>
+  @endif
+</div>
+
+<div class="col-sm-4">
+  @php
+  $myview = App\Bucket::where('roll', Auth::user()->rollno)->where('list', 8)->get()->toArray();
+  @endphp
+  @if(!empty($myview))
+  <aside class="profile-card" style="background: url({{$myview[0]['pic']}}); background-size: 100% 100%; background-repeat: no-repeat;">
+    <header>
+      <h1>Bonfire</h1><i class="fa fa-check-circle" style="color: #5cb85c; font-size: 30px;" title="Task Completed!"></i>
+      <h2>Jolly Bonfire at Beach</h2>
+    </header>
+    <div class="profile-bio" style="text-align: center;">
+      <form action="/bucketpost/8" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="file" required="required" name="fileToUpload" >
+        <br><br>
+        <input type="submit" value="Change" name="submit" class="btn btn-success" align="right">
+      </form>
+    </div>
+  </aside><br>
+  @else
+  <aside class="profile-card" style="background: url(https://s-media-cache-ak0.pinimg.com/736x/3a/1f/02/3a1f02c10700daa80655a302c5a27acf.jpg);">
+    <header>
+      <h1>Bonfire</h1>
+      <h2>Jolly Bonfire at Beach</h2>
+    </header>
+    <div class="profile-bio" style="text-align: center;">
+      <form action="/bucketpost/8" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="file" required="required" name="fileToUpload" >
+        <br><br>
+        <input type="submit" value="Upload Image" name="submit" class="btn btn-success" align="right">
+      </form>
+    </div>
+  </aside><br>
+  @endif
+</div>
+
+<div class="col-sm-4">
+  @php
+  $myview = App\Bucket::where('roll', Auth::user()->rollno)->where('list', 9)->get()->toArray();
+  @endphp
+  @if(!empty($myview))
+  <aside class="profile-card" style="background: url({{$myview[0]['pic']}}); background-size: 100% 100%; background-repeat: no-repeat;">
+    <header>
+      <h1>Little Sisters</h1><i class="fa fa-check-circle" style="color: #5cb85c; font-size: 30px;" title="Task Completed!"></i>
+      <h2>“Delicacies” at Little Sisters</h2>
+    </header>
+    <div class="profile-bio" style="text-align: center;">
+      <form action="/bucketpost/9" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="file" required="required" name="fileToUpload" >
+        <br><br>
+        <input type="submit" value="Change" name="submit" class="btn btn-success" align="right">
+      </form>
+    </div>
+  </aside><br>
+  @else
+  <aside class="profile-card" style="background: url(https://s-media-cache-ak0.pinimg.com/736x/3a/1f/02/3a1f02c10700daa80655a302c5a27acf.jpg);">
+    <header>
+      <h1>Little Sisters</h1>
+      <h2>“Delicacies” at Little Sisters</h2>
+    </header>
+    <div class="profile-bio" style="text-align: center;">
+      <form action="/bucketpost/9" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="file" required="required" name="fileToUpload" >
+        <br><br>
+        <input type="submit" value="Upload Image" name="submit" class="btn btn-success" align="right">
+      </form>
+    </div>
+  </aside><br>
+  @endif
+</div>
